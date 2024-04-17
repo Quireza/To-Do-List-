a680d5cab42bd20f76ca70f1d40fcb798a28f09a
Quireza <daniel_quireza12@hotmail.com>
2024-04-17 13:07:42 -0300

    any details for my app

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let tasks = [];
+const tasks = [];
+let count = 0;
 
 function callMyTask() {
   let myButton = document.getElementById('submit');
@@ -9,15 +10,35 @@ function callMyTask() {
     let createMyTask = document.createElement('li');
     let checkbox = document.createElement('input');
     let label = document.createElement('label');
+    let numberTasksClosed = document.getElementById('numberTasksClosed');
     
     checkbox.type = 'checkbox';
     checkbox.name = 'checkbox';
+    checkbox.id = 'checkboxId';
+
+    checkbox.addEventListener('change', function() {
+      if(this.checked) {
+
+        let taskIndex = tasks.indexOf(label.textContent);
+        if (taskIndex !== -1) {
+          tasks.splice(taskIndex, 1);
+          count += 1;
+          numberTasksClosed.textContent = count;
+        }
+
+        createMyTask.remove();
+
+        if (tasks.length === 0) {
+          emptyTask.style.display = 'block';
+        }
+      }
+    });
 
     if (tasks.length !== '0') {
       let emptyTask = document.getElementById('emptyTask');
 
       emptyTask.style.display = 'none';
-    }
+    } 
     
     if (myTextTask == '') {
       return false
@@ -26,12 +47,22 @@ function callMyTask() {
       createMyTask.appendChild(checkbox);
       createMyTask.appendChild(label);
       myListTask.appendChild(createMyTask);
-
       tasks.push(myTextTask);
+
     }
 
     console.log(tasks);
+    console.log(count);
 
     document.getElementById('myTask').value = '';
+  })
+}
+
+
+function showTasksCLosed() {
+  numberTasksClosed.addEventListener('click', () => {
+    let containerClosed = document.getElementById('containerClosed');
+
+    containerClosed.style.display = 'block';
   })
 }
